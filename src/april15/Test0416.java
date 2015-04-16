4fa05a7f48a6499ceb7c659bbac2e499f2211c29
x14g023 <x14g023@PC505N022.chiba-fjb.ac.jp>
2015-04-16 15:01:28 +0900

    主命とあらばコミットでもこなしますよ

---
+++ src/april15/Test0416.java
@@ -0,0 +1,14 @@
+package april15;
+
+public class Test0416 {
+	public static void main(String args[]) {
+		// // デバッグ
+		// int i;
+		// // iを0に初期設定;i<3を満たしていたら処理を実行;処理終了後、iに1を足す
+		// for (i = 0; i < 3; i = i + 1)
+		// System.out.println(i); // iの値を出力し改行
+
+
+	}
+
+}
